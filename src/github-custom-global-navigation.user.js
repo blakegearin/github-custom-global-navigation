982ed5c6fd1899b5c6ae3497f2aba7a75c46cbb4
Blake Gearin <blakegearin@pm.me>
2024-01-29 23:25:22 -0700

    Update slash icon in search bar from URL to an SVG

--- src/github-custom-global-navigation.user.js
+++ src/github-custom-global-navigation.user.js
@@ -435,10 +435,13 @@
         }
       `;
 
-      const slashImg = document.createElement('img');
-      slashImg.src = 'https://github.githubassets.com/images/search-key-slash.svg';
+      const parser = new DOMParser();
+      const svgDoc = parser.parseFromString(
+        '<svg xmlns="http://www.w3.org/2000/svg" width="22" height="20" aria-hidden="true"><path fill="none" stroke="#979A9C" opacity=".4" d="M3.5.5h12c1.7 0 3 1.3 3 3v13c0 1.7-1.3 3-3 3h-12c-1.7 0-3-1.3-3-3v-13c0-1.7 1.3-3 3-3z"></path><path fill="#979A9C" d="M11.8 6L8 15.1h-.9L10.8 6h1z"></path></svg>',
+        'image/svg+xml',
+      );
+      const slashImg = svgDoc.documentElement;
       slashImg.alt = 'slash key to search';
-      slashImg.className = 'header-search-key-slash';
 
       const placeholderDiv = HEADER.querySelector(elementSelector.placeholderDiv);
 
