2bae34105c5440ee1cdfae496d69c3412805be7e
Blake Gearin <hello@blakeg.me>
2025-03-29 22:27:30 -0600

    Release commit for 1.6.10

--- src/github-custom-global-navigation.user.js
+++ src/github-custom-global-navigation.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GitHub Custom Global Navigation
 // @namespace    https://github.com/blakegearin/github-custom-global-navigation
-// @version      1.6.9
+// @version      1.6.10
 // @description  Customize GitHub's new global navigation
 // @author       Blake Gearin <hello@blakeg.me> (https://blakegearin.com)
 // @match        https://github.com/*
@@ -20,7 +20,7 @@
 (function () {
   'use strict';
 
-  const VERSION = '1.6.9';
+  const VERSION = '1.6.10';
 
   const SILENT = 0;
   const QUIET = 1;
