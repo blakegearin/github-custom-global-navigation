5ab9d1aff328125bd3fe6387fbb070ed7c40c45e
Blake Gearin <hello@blakeg.me>
2025-09-10 01:58:48 -0600

    Fix menu item adding

--- src/github-custom-global-navigation.user.js
+++ src/github-custom-global-navigation.user.js
@@ -2168,92 +2168,89 @@
     };
   }
 
-  function waitForFeaturePreviewButton() {
-    log(VERBOSE, 'waitForFeaturePreviewButton()');
-
-    if (!HEADER) return;
+  function addMenuItemToActionList() {
+    log(VERBOSE, 'addMenuItemToActionList()');
 
     const liElementId = 'custom-global-navigation-menu-item';
 
-    if (HEADER.querySelector(createId(liElementId))) return;
+    if (document.querySelector(createId(liElementId))) return;
 
-    const featurePreviewSearch = Array.from(
-      document.querySelectorAll('[data-position-regular="right"] span'),
-    )?.find(element => element.textContent === 'Feature preview') || null;
-
-    if (featurePreviewSearch) {
-      const featurePreviewSpan = featurePreviewSearch;
-      const featurePreviewLabelDiv = featurePreviewSpan.parentNode;
-      const featurePreviewLi = featurePreviewLabelDiv.parentNode;
-
-      const newLiElement = featurePreviewLi.cloneNode(true);
-      newLiElement.setAttribute('id', liElementId);
-
-      newLiElement.onclick = () => {
-        const closeButton = document.querySelector(SELECTORS.sidebars.right.closeButton);
-        if (!closeButton) {
-          logError(`Selector '${SELECTORS.sidebars.right.closeButton}' not found`);
-        } else {
-          closeButton.click();
-        }
-
-        GMC.open();
-      };
-
-      const textElement = newLiElement.querySelector('button > span > span');
-      textElement.textContent = gmcGet('menu_item_title');
-
-      const oldSvg = newLiElement.querySelector('svg');
-
-      const menuItemIcon = gmcGet('menu_item_icon');
-      if (menuItemIcon === 'logo') {
-        const newSvg = document.createElement('img');
-        newSvg.setAttribute('height', '16px');
-        newSvg.setAttribute('width', '16px');
-        newSvg.src = `https://raw.githubusercontent.com/blakegearin/github-custom-global-navigation/main/img/${THEME}_logo.svg`;
-
-        oldSvg.parentNode.replaceChild(newSvg, oldSvg);
-      } else {
-        let svgString;
-
-        if (menuItemIcon === 'cog') {
-          svgString = `
-            <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-gear">
-              <path d="M8 0a8.2 8.2 0 0 1 .701.031C9.444.095 9.99.645 10.16 1.29l.288 1.107c.018.066.079.158.212.224.231.114.454.243.668.386.123.082.233.09.299.071l1.103-.303c.644-.176 1.392.021 1.82.63.27.385.506.792.704 1.218.315.675.111 1.422-.364 1.891l-.814.806c-.049.048-.098.147-.088.294.016.257.016.515 0 .772-.01.147.038.246.088.294l.814.806c.475.469.679 1.216.364 1.891a7.977 7.977 0 0 1-.704 1.217c-.428.61-1.176.807-1.82.63l-1.102-.302c-.067-.019-.177-.011-.3.071a5.909 5.909 0 0 1-.668.386c-.133.066-.194.158-.211.224l-.29 1.106c-.168.646-.715 1.196-1.458 1.26a8.006 8.006 0 0 1-1.402 0c-.743-.064-1.289-.614-1.458-1.26l-.289-1.106c-.018-.066-.079-.158-.212-.224a5.738 5.738 0 0 1-.668-.386c-.123-.082-.233-.09-.299-.071l-1.103.303c-.644.176-1.392-.021-1.82-.63a8.12 8.12 0 0 1-.704-1.218c-.315-.675-.111-1.422.363-1.891l.815-.806c.05-.048.098-.147.088-.294a6.214 6.214 0 0 1 0-.772c.01-.147-.038-.246-.088-.294l-.815-.806C.635 6.045.431 5.298.746 4.623a7.92 7.92 0 0 1 .704-1.217c.428-.61 1.176-.807 1.82-.63l1.102.302c.067.019.177.011.3-.071.214-.143.437-.272.668-.386.133-.066.194-.158.211-.224l.29-1.106C6.009.645 6.556.095 7.299.03 7.53.01 7.764 0 8 0Zm-.571 1.525c-.036.003-.108.036-.137.146l-.289 1.105c-.147.561-.549.967-.998 1.189-.173.086-.34.183-.5.29-.417.278-.97.423-1.529.27l-1.103-.303c-.109-.03-.175.016-.195.045-.22.312-.412.644-.573.99-.014.031-.021.11.059.19l.815.806c.411.406.562.957.53 1.456a4.709 4.709 0 0 0 0 .582c.032.499-.119 1.05-.53 1.456l-.815.806c-.081.08-.073.159-.059.19.162.346.353.677.573.989.02.03.085.076.195.046l1.102-.303c.56-.153 1.113-.008 1.53.27.161.107.328.204.501.29.447.222.85.629.997 1.189l.289 1.105c.029.109.101.143.137.146a6.6 6.6 0 0 0 1.142 0c.036-.003.108-.036.137-.146l.289-1.105c.147-.561.549-.967.998-1.189.173-.086.34-.183.5-.29.417-.278.97-.423 1.529-.27l1.103.303c.109.029.175-.016.195-.045.22-.313.411-.644.573-.99.014-.031.021-.11-.059-.19l-.815-.806c-.411-.406-.562-.957-.53-1.456a4.709 4.709 0 0 0 0-.582c-.032-.499.119-1.05.53-1.456l.815-.806c.081-.08.073-.159.059-.19a6.464 6.464 0 0 0-.573-.989c-.02-.03-.085-.076-.195-.046l-1.102.303c-.56.153-1.113.008-1.53-.27a4.44 4.44 0 0 0-.501-.29c-.447-.222-.85-.629-.997-1.189l-.289-1.105c-.029-.11-.101-.143-.137-.146a6.6 6.6 0 0 0-1.142 0ZM11 8a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM9.5 8a1.5 1.5 0 1 0-3.001.001A1.5 1.5 0 0 0 9.5 8Z"></path>
-            </svg>
-          `;
-        } else if (menuItemIcon === 'compass') {
-          svgString = `
-            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
-              <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
-              <path d="M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm306.7 69.1L162.4 380.6c-19.4 7.5-38.5-11.6-31-31l55.5-144.3c3.3-8.5 9.9-15.1 18.4-18.4l144.3-55.5c19.4-7.5 38.5 11.6 31 31L325.1 306.7c-3.2 8.5-9.9 15.1-18.4 18.4zM288 256a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z"/>
-            </svg>
-          `;
-        }
-
-        const parser = new DOMParser();
-        const svgDoc = parser.parseFromString(svgString, 'image/svg+xml');
-        const newSvg = svgDoc.documentElement;
-
-        oldSvg.parentNode.replaceChild(newSvg, oldSvg);
-      }
-
-      const parentUl = featurePreviewLi.parentNode;
-      const settingsLi = document.querySelector('[data-position-regular="right"] a[href="/settings/profile"]').parentNode;
-
-      parentUl.insertBefore(newLiElement, settingsLi.nextSibling);
-
-      const divider = featurePreviewLi.parentNode.querySelector(SELECTORS.sidebars.right.divider);
-      if (!divider) {
-        logError(`Selector '${SELECTORS.sidebars.right.divider}' not found`);
-        return;
-      }
-      const newDivider = divider.cloneNode(true);
-
-      parentUl.insertBefore(newDivider, settingsLi.nextSibling);
-    } else {
-      setTimeout(waitForFeaturePreviewButton, 100);
+    const actionList = document.querySelector(SELECTORS.sidebars.right.actionList);
+    if (!actionList) {
+      log(DEBUG, 'Action list not found');
+      return setTimeout(addMenuItemToActionList, 100);
     }
+
+    const signOutLink = actionList.querySelector(SELECTORS.sidebars.right.signOutLink);
+    if (!signOutLink) {
+      log(DEBUG, 'Logout link not found');
+      return;
+    }
+
+    const signOutLi = signOutLink.parentNode;
+
+    const newLiElement = signOutLi.cloneNode(true);
+    newLiElement.setAttribute('id', liElementId);
+    newLiElement.querySelector('a').removeAttribute('href');
+
+    newLiElement.onclick = () => {
+      const closeButton = document.querySelector(SELECTORS.sidebars.right.closeButton);
+      if (!closeButton) {
+        logError(`Selector '${SELECTORS.sidebars.right.closeButton}' not found`);
+      } else {
+        closeButton.click();
+      }
+
+      GMC.open();
+    };
+
+    const textElement = newLiElement.querySelector('a > [class^="prc-ActionList-ActionListSubContent-"]');
+    textElement.textContent = gmcGet('menu_item_title');
+
+    const oldSvg = newLiElement.querySelector('svg');
+
+    const menuItemIcon = gmcGet('menu_item_icon');
+    if (menuItemIcon === 'logo') {
+      const newSvg = document.createElement('img');
+      newSvg.setAttribute('height', '16px');
+      newSvg.setAttribute('width', '16px');
+      newSvg.src = `https://raw.githubusercontent.com/blakegearin/github-custom-global-navigation/main/img/${THEME}_logo.svg`;
+
+      oldSvg.parentNode.replaceChild(newSvg, oldSvg);
+    } else {
+      let svgString;
+
+      if (menuItemIcon === 'cog') {
+        svgString = `
+          <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-gear">
+            <path d="M8 0a8.2 8.2 0 0 1 .701.031C9.444.095 9.99.645 10.16 1.29l.288 1.107c.018.066.079.158.212.224.231.114.454.243.668.386.123.082.233.09.299.071l1.103-.303c.644-.176 1.392.021 1.82.63.27.385.506.792.704 1.218.315.675.111 1.422-.364 1.891l-.814.806c-.049.048-.098.147-.088.294.016.257.016.515 0 .772-.01.147.038.246.088.294l.814.806c.475.469.679 1.216.364 1.891a7.977 7.977 0 0 1-.704 1.217c-.428.61-1.176.807-1.82.63l-1.102-.302c-.067-.019-.177-.011-.3.071a5.909 5.909 0 0 1-.668.386c-.133.066-.194.158-.211.224l-.29 1.106c-.168.646-.715 1.196-1.458 1.26a8.006 8.006 0 0 1-1.402 0c-.743-.064-1.289-.614-1.458-1.26l-.289-1.106c-.018-.066-.079-.158-.212-.224a5.738 5.738 0 0 1-.668-.386c-.123-.082-.233-.09-.299-.071l-1.103.303c-.644.176-1.392-.021-1.82-.63a8.12 8.12 0 0 1-.704-1.218c-.315-.675-.111-1.422.363-1.891l.815-.806c.05-.048.098-.147.088-.294a6.214 6.214 0 0 1 0-.772c.01-.147-.038-.246-.088-.294l-.815-.806C.635 6.045.431 5.298.746 4.623a7.92 7.92 0 0 1 .704-1.217c.428-.61 1.176-.807 1.82-.63l1.102.302c.067.019.177.011.3-.071.214-.143.437-.272.668-.386.133-.066.194-.158.211-.224l.29-1.106C6.009.645 6.556.095 7.299.03 7.53.01 7.764 0 8 0Zm-.571 1.525c-.036.003-.108.036-.137.146l-.289 1.105c-.147.561-.549.967-.998 1.189-.173.086-.34.183-.5.29-.417.278-.97.423-1.529.27l-1.103-.303c-.109-.03-.175.016-.195.045-.22.312-.412.644-.573.99-.014.031-.021.11.059.19l.815.806c.411.406.562.957.53 1.456a4.709 4.709 0 0 0 0 .582c.032.499-.119 1.05-.53 1.456l-.815.806c-.081.08-.073.159-.059.19.162.346.353.677.573.989.02.03.085.076.195.046l1.102-.303c.56-.153 1.113-.008 1.53.27.161.107.328.204.501.29.447.222.85.629.997 1.189l.289 1.105c.029.109.101.143.137.146a6.6 6.6 0 0 0 1.142 0c.036-.003.108-.036.137-.146l.289-1.105c.147-.561.549-.967.998-1.189.173-.086.34-.183.5-.29.417-.278.97-.423 1.529-.27l1.103.303c.109.029.175-.016.195-.045.22-.313.411-.644.573-.99.014-.031.021-.11-.059-.19l-.815-.806c-.411-.406-.562-.957-.53-1.456a4.709 4.709 0 0 0 0-.582c-.032-.499.119-1.05.53-1.456l.815-.806c.081-.08.073-.159.059-.19a6.464 6.464 0 0 0-.573-.989c-.02-.03-.085-.076-.195-.046l-1.102.303c-.56.153-1.113.008-1.53-.27a4.44 4.44 0 0 0-.501-.29c-.447-.222-.85-.629-.997-1.189l-.289-1.105c-.029-.11-.101-.143-.137-.146a6.6 6.6 0 0 0-1.142 0ZM11 8a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM9.5 8a1.5 1.5 0 1 0-3.001.001A1.5 1.5 0 0 0 9.5 8Z"></path>
+          </svg>
+        `;
+      } else if (menuItemIcon === 'compass') {
+        svgString = `
+          <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
+            <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+            <path d="M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm306.7 69.1L162.4 380.6c-19.4 7.5-38.5-11.6-31-31l55.5-144.3c3.3-8.5 9.9-15.1 18.4-18.4l144.3-55.5c19.4-7.5 38.5 11.6 31 31L325.1 306.7c-3.2 8.5-9.9 15.1-18.4 18.4zM288 256a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z"/>
+          </svg>
+        `;
+      }
+
+      const parser = new DOMParser();
+      const svgDoc = parser.parseFromString(svgString, 'image/svg+xml');
+      const newSvg = svgDoc.documentElement;
+
+      oldSvg.parentNode.replaceChild(newSvg, oldSvg);
+    }
+
+    const divider = signOutLi.parentNode.querySelector(SELECTORS.sidebars.right.divider);
+    if (!divider) {
+      logError(`Selector '${SELECTORS.sidebars.right.divider}' not found`);
+      return;
+    }
+    const newDivider = divider.cloneNode(true);
+
+    actionList.insertBefore(newLiElement, signOutLi);
+    actionList.insertBefore(newDivider, signOutLi);
   }
 
   function generateCustomConfig() {
@@ -3309,14 +3306,14 @@
 
     if (!HEADER) return 'continue';
 
-    const featurePreviewButton = document.querySelector(SELECTORS.avatar.button);
+    const avatarButton = document.querySelector(SELECTORS.avatar.button);
 
-    if (!featurePreviewButton) {
+    if (!avatarButton) {
       logError(`Selector ${SELECTORS.avatar.button} not found`);
       return 'break';
     }
 
-    featurePreviewButton.addEventListener('click', waitForFeaturePreviewButton);
+    avatarButton.addEventListener('click', addMenuItemToActionList);
 
     CONFIG_NAME = {
       'Off': 'off',
@@ -3643,6 +3640,8 @@
         modalDialog: '#__primerPortalRoot__ > div > [data-position-regular="right"] > div',
         closeButton: '#__primerPortalRoot__ button[aria-label="Close"]',
         divider: 'li[data-component="ActionList.Divider"]',
+        actionList: '[class^="prc-ActionList-"]',
+        signOutLink: '[class^="prc-ActionList-"] a[href="/logout"]',
       },
     },
   };
