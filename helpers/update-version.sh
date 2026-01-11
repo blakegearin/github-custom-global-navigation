1ea344b1913d38cb1c5a77c992b5d16f2899fa66
Blake Gearin <hello@blakeg.me>
2026-01-10 23:29:21 -0700

    Add copyright year updating

--- helpers/update-version.sh
+++ helpers/update-version.sh
@@ -27,9 +27,13 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
-if [ $? -eq 0 ]; then
-  echo "Version updated to $NEW_VERSION in $FILE"
-else
-  echo "Failed to update version in $FILE"
+# Update copyright end year if needed
+CURRENT_YEAR=$(date +%Y)
+# This will match lines like: // @copyright    2023–2025
+sed -i '' -E "s|(// @copyright[[:space:]]+[0-9]{4})–[0-9]{4}|\1–$CURRENT_YEAR|" "$FILE"
+if [ $? -ne 0 ]; then
+  echo "Failed to update copyright year. Aborting."
   exit 1
 fi
+
+echo "Version updated to $NEW_VERSION and copyright year set to $CURRENT_YEAR in $FILE"
